94544720bf2c3a7bae6ac7d5bda098e23699dafb
Simon Walther <simon.walther@liquid-concept.ch>
2014-05-19 17:01:43 +0200

    do tests which run proprely

--- latintointeger.rb
+++ latintointeger.rb
@@ -1,67 +1,74 @@
 module LatinToInteger
-  class Converter
-    attr_reader :integerNumber
 
-    def initialize
-      letterArray = ["M", "D", "C", "L", "X", "V", "I"]
-      @letterArray = letterArray
-      valueArray = [1000, 500, 100, 50, 10, 5, 1]
-      @valueArray = valueArray
-    end
+  LetterArray = ["M", "D", "C", "L", "X", "V", "I"]
+  ValueArray = [1000, 500, 100, 50, 10, 5, 1]
 
-    def convert(latinNumber)
-      integerNumber = 0
-      latinNumber = latinNumber.upcase.split(//)
+  def convert(latinNumber)
+    integerNumber = 0
+    latinNumber = latinNumber.upcase.split(//)
 
-      latinNumber.length.times do |i|
-        #if the letter exists
-        if @letterArray.include?(latinNumber[i])
-          #if the letter is different from "I"
-          if latinNumber[i] != @letterArray.last
-            #if the letter is "M" or "C" or "X"
-            if (@letterArray.index(latinNumber[i])+2)%2 == 0
-              #if the previous letter is two units after
-              if (i-1) >= 0 && @letterArray.index(latinNumber[i]) == (@letterArray.index(latinNumber[i-1])-2)
-                #it subtract 2 times the value of the previous letter to the value of the curret letter and add the result to integerNumber
-                integerNumber += (@valueArray[@letterArray.index(latinNumber[i])]-(@valueArray[@letterArray.index(latinNumber[i-1])])*2)
-                #if the previous letter isn't two units after
-              else
-                #it add the value of the letter to integerNumber
-                integerNumber += @valueArray[@letterArray.index(latinNumber[i])]
-              end
-            #if the letter is "D" or "L" or "V" and the letter is different from "I"
-            elsif (@letterArray.index(latinNumber[i])+2)%2 == 1
-              #if the previous letter is one unit after
-              if (i-1) >= 0 && @letterArray.index(latinNumber[i]) == (@letterArray.index(latinNumber[i-1])-1)
-                #it subtract 2 times the value of the previous letter to the value of the curret letter and add the result to integerNumber
-                integerNumber += (@valueArray[@letterArray.index(latinNumber[i])]-(@valueArray[@letterArray.index(latinNumber[i-1])])*2)
-              #if the previous letter isn't one unit after
-              else
-                #it add the value of the letter to integerNumber
-                integerNumber += @valueArray[@letterArray.index(latinNumber[i])]
-              end
+    latinNumber.length.times do |i|
+      #if the letter exists
+      if latinNumber != nil && LetterArray.include?(latinNumber[i]) == false
+        raise "the letter #{latinNumber[i]} isn't a latin number\nlatin numbers are: #{LetterArray.join(" ")}"
+      else
+        #if the letter is different from "I"
+        if latinNumber[i] != LetterArray.last
+          #if the letter is "M" or "C" or "X"
+          if (LetterArray.index(latinNumber[i])+2)%2 == 0
+            #if the previous letter index is bigger by two than the current letter index
+            if (i-1) >= 0 && LetterArray.index(latinNumber[i]) == (LetterArray.index(latinNumber[i-1])-2)
+              #it subtract 2 times the value of the previous letter to the value of the curret letter and add the result to integerNumber
+              integerNumber += (ValueArray[LetterArray.index(latinNumber[i])]-(ValueArray[LetterArray.index(latinNumber[i-1])])*2)
+            #if there's 4 times or more the same letter and if the letter isn't M
+            elsif (i-3) >= 0 && LetterArray.index(latinNumber[i]) != 0 && LetterArray.index(latinNumber[i-3]) == LetterArray.index(latinNumber[i-2]) && latinNumber[i-2] == latinNumber[i-1] && latinNumber[i-1] == latinNumber[i]
+              #it raise an error
+              raise "#{latinNumber[i-3]}#{latinNumber[i-2]}#{latinNumber[i-1]}#{latinNumber[i]} isn't a correct combination (#{latinNumber[i]}#{LetterArray[(LetterArray.index(latinNumber[i]))-1]} is) #1"
+            #if there's five or more times the letter M
+            elsif (i-4) >= 0 && LetterArray.index(latinNumber[i]) == 0 && LetterArray.index(latinNumber[i-4]) == LetterArray.index(latinNumber[i-3]) && latinNumber[i-3] == latinNumber[i-2] && latinNumber[i-2] == latinNumber[i-1] && latinNumber[i-1] == latinNumber[i]
+              #it raise an error
+              raise "MMMMM isn't a correct combination: latin numbers exists up to 4999 (without lines over letter)"
+            #if the previous letter index is 1 or 3 bigger than the current letter index
+            elsif (i-1) >= 0 && ((LetterArray.index(latinNumber[i])-LetterArray.index(latinNumber[i-1])) == -1 || (LetterArray.index(latinNumber[i])-LetterArray.index(latinNumber[i-1])) <= -3)
+              #it raise an error
+              raise "#{latinNumber[i-1]}#{latinNumber[i]} isn't a correct combination #1"
+            #if the previous letter index is bigger than the current letter and more one than one time before it
+            elsif (i-3) >= 0 && LetterArray.index(latinNumber[i-1]) == LetterArray.index(latinNumber[i-2]) && LetterArray.index(latinNumber[i-1]) > LetterArray.index(latinNumber[i])
+            #if the previous letter index isn't bigger by two than the current letter index
+            else
+              #it add the value of the letter to integerNumber
+              integerNumber += ValueArray[LetterArray.index(latinNumber[i])]
             end
-          #if the letter is "I"
+          #if the letter is "D" or "L" or "V" and the letter is different from "I"
+          elsif (LetterArray.index(latinNumber[i])+2)%2 == 1
+            #if the previous letter index is bigger by 1 than the current letter
+            if (i-1) >= 0 && LetterArray.index(latinNumber[i]) == (LetterArray.index(latinNumber[i-1])-1)
+              #it subtract 2 times the value of the previous letter to the value of the curret letter and add the result to integerNumber
+              integerNumber += (ValueArray[LetterArray.index(latinNumber[i])]-(ValueArray[LetterArray.index(latinNumber[i-1])])*2)
+            #if the previous letter is the same letter or if the previous letter index is bigger by two than the current letter index
+            elsif (i-1) >= 0 && latinNumber[i] == latinNumber[i-1] || (LetterArray.index(latinNumber[i])-LetterArray.index(latinNumber[i-1])) <= -2
+              #it raise an error
+              raise "#{latinNumber[i-1]}#{latinNumber[i]} isn't a correct combination #2"
+            #if the previous letter isn't the same
+            else
+              #it add the value of the letter to integerNumber
+              integerNumber += ValueArray[LetterArray.index(latinNumber[i])]
+            end
+          end
+        #if the letter is "I"
+        else
+          #if there's 4 times or more the same letter
+          if (i-3) >= 0 && latinNumber[i-3] == latinNumber[i-2] && latinNumber[i-2] == latinNumber[i-1] && latinNumber[i-1] = latinNumber[i]
+            #it raise an error
+            raise "#{latinNumber[i-3]}#{latinNumber[i-2]}#{latinNumber[i-1]}#{latinNumber[i]} isn't a correct combination (#{latinNumber[i]}#{LetterArray[(LetterArray.index(latinNumber[i]))-1]} is) #2"
           else
             #it add 1 to integerNumber
             integerNumber += 1
           end
-        #if the letter doesn't exists
-        else
-          puts "the letter #{latinNumber[i]} is not a latin number"
-          puts "latin numbers are: #{@letterArray.join(" ")}"
-          exit(1)
         end
       end
-
-      self.getResult(integerNumber)
     end
 
-    def getResult(integerNumber)
-      puts "the result #{integerNumber}"
-    end
+    integerNumber
   end
-
-  converter = Converter.new
-  converter.convert("MMMDCXLIV")
 end
--- spec/latintointeger_spec.rb
+++ spec/latintointeger_spec.rb
@@ -1,1 +1,203 @@
-require '../latintointeger'
+require 'spec_helper'
+require 'latintointeger'
+
+class Test
+  include LatinToInteger
+end
+
+describe LatinToInteger do
+  subject { Test.new }
+
+  describe 'test: letters' do
+    it 'M should have the value of 1000' do
+      result = subject.convert("M")
+      result.should == 1000
+    end
+
+    it 'D should have the value of 500' do
+      result = subject.convert("D")
+      result.should == 500
+    end
+
+    it 'C should have the value of 100' do
+      result = subject.convert("C")
+      result.should == 100
+    end
+
+    it 'L should have the value of 50' do
+      result = subject.convert("L")
+      result.should == 50
+    end
+
+    it 'X should have the value of 10' do
+      result = subject.convert("X")
+      result.should == 10
+    end
+
+    it 'V should have the value of 5' do
+      result = subject.convert("V")
+      result.should == 5
+    end
+
+    it 'I should have the value of 1' do
+      result = subject.convert("I")
+      result.should == 1
+    end
+  end
+
+  describe 'test: 9, 99, 999, 4999:' do
+    it 'IX should have the value of 9' do
+      result = subject.convert("IX")
+      result.should == 9
+    end
+
+    it 'XCIX should have the value of 99' do
+      result = subject.convert("XCIX")
+      result.should == 99
+    end
+
+    it 'CMXCIX should have the value of 999' do
+      result = subject.convert("CMXCIX")
+      result.should == 999
+    end
+
+    it 'MMMMCMXCIX should have the value of 4999' do
+      result = subject.convert("MMMMCMXCIX")
+      result.should == 4999
+    end
+  end
+
+  describe 'test: letters combinations' do
+    it 'CM should have the value of 900' do
+      result = subject.convert("CM")
+      result.should == 900
+    end
+
+    it 'CD should have the value of 400' do
+      result = subject.convert("CD")
+      result.should == 400
+    end
+
+    it 'XC should have the value of 90' do
+      result = subject.convert("XC")
+      result.should == 90
+    end
+
+    it 'XL should have the value of 40' do
+      result = subject.convert("XL")
+      result.should == 40
+    end
+
+    it 'IX should have the value of 9' do
+      result = subject.convert("IX")
+      result.should == 9
+    end
+
+    it 'IV should have the value of 4' do
+      result = subject.convert("IV")
+      result.should == 4
+    end
+
+    describe 'without correct combinations:' do
+      it "DM shouldn't be possible" do
+        expect { subject.convert("DM")}.to raise_error
+      end
+
+      it "LD shouldn't be possible" do
+        expect { subject.convert("LD")}.to raise_error
+      end
+
+      it "IC shouldn't be possible" do
+        expect { subject.convert("IC")}.to raise_error
+      end
+
+      it "XM shouldn't be possible" do
+        expect { subject.convert("XM")}.to raise_error
+      end
+
+      it "LV shouldn't be possible" do
+        expect { subject.convert("LV")}.to raise_error
+      end
+
+      it "IIIV shouldn't be possible" do
+        expect { subject.convert("IIIV")}.to raise_error
+      end
+
+      it "XXL shouldn't be possible" do
+        expect { subject.convert("XXL")}.to raise_error
+      end
+
+      it "XXXC shouldn't be possible" do
+        expect { subject.convert("XXXC")}.to raise_error
+      end
+
+      it "CCD shouldn't be possible" do
+        expect { subject.convert("CCD")}.to raise_error
+      end
+
+      it "CCCM shouldn't be possible" do
+        expect { subject.convert("CCCM")}.to raise_error
+      end
+    end
+
+    describe 'with multiple times the same letter:' do
+      describe 'with M C X or I:' do
+        describe 'with less than 4 times the same letter:' do
+          it 'MMM should have the value of 3000' do
+            result = subject.convert("MMM")
+            result.should == 3000
+          end
+
+          it 'CCC should have the value of 300' do
+            result = subject.convert("CCC")
+            result.should == 300
+          end
+
+          it 'XXX should have the value of 30' do
+            result = subject.convert("XXX")
+            result.should == 30
+          end
+
+          it 'III should have the value of 3' do
+            result = subject.convert("III")
+            result.should == 3
+          end
+        end
+
+        describe 'with 4 times or more the same letter:' do
+          it "IIII shouldn't be possible" do
+            expect { subject.convert("IIII")}.to raise_error
+          end
+
+          it "XXXXX shouldn't be possible" do
+            expect { subject.convert("XXXXX")}.to raise_error
+          end
+
+          it "CCCCCC shouldn't be possible" do
+            expect { subject.convert("CCCCCC")}.to raise_error
+          end
+
+          it "MMMMMMM shouldn't be possible" do
+            expect { subject.convert("MMMMMMM")}.to raise_error
+          end
+        end
+      end
+
+      describe 'with D L or V:' do
+        describe 'with multiple times the same letter' do
+          it "DDD shouldn't be possible" do
+            expect { subject.convert("DDD")}.to raise_error
+          end
+
+          it "LLL shouldn't be possible" do
+            expect { subject.convert("LLL")}.to raise_error
+          end
+
+          it "VVV shouldn't be possible" do
+            expect { subject.convert("VVV")}.to raise_error
+          end
+        end
+      end
+    end
+  end
+end
