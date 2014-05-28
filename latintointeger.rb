f43b1cb655a73454db06b5fcfb819e2b20330ed7
Simon Walther <simon.walther@liquid-concept.ch>
2014-05-28 14:49:45 +0200

    move conditions into methods

--- latintointeger.rb
+++ latintointeger.rb
@@ -4,16 +4,16 @@ module LatinToInteger
 
   def latin_to_integer(latin_number)
     latin_number = latin_number.upcase.split(//).reverse
-    self.verify(latin_number)
-    self.calculate(latin_number)
+    verify(latin_number)
+    calculate(latin_number)
   end
 
   def verify(latin_number)
     same_letter_counter = 0
 
     latin_number.each_with_index do |current_latin_number, i|
-      group_current_letter = (self.group_of(current_latin_number))
-      group_next_letter = (self.group_of(latin_number[i-1]))
+      group_current_letter = (group_of(current_latin_number))
+      group_next_letter = (group_of(latin_number[i-1]))
 
       if i > 0
         #count same letter
@@ -24,12 +24,16 @@ module LatinToInteger
         end
 
         #verify if the letter can be this amount of time
-        if (group_current_letter == "X" && same_letter_counter > 2 && current_latin_number != LETTERARRAY.last) || (group_current_letter == "V" && same_letter_counter > 0) || (current_latin_number == LETTERARRAY.last && same_letter_counter > 3)
+        if too_many_times_group_x(group_current_letter, same_letter_counter, current_latin_number) ||
+           too_many_times_group_v(group_current_letter, same_letter_counter) ||
+           too_many_times_letter_m(current_latin_number, same_letter_counter)
           raise "there's more than #{same_letter_counter} times the letter #{current_latin_number} !"
         end
 
         #verify if letters are compatible
-        if (group_current_letter == "X" && (LETTERARRAY.index(latin_number[i-1])-LETTERARRAY.index(current_latin_number) >= 3)) || (group_current_letter == "V" && ((LETTERARRAY.index(current_latin_number) < LETTERARRAY.index(latin_number[i-1])) || (group_next_letter == "V")))
+        if is_next_letter_incompatible_for_group_x(group_current_letter, current_latin_number, latin_number, i) ||
+           is_next_letter_incompatible_for_group_v(group_current_letter, current_latin_number, latin_number, i) ||
+           is_two_letter_of_group_v(group_current_letter, group_next_letter)
           raise "#{current_latin_number} is a letter incompatible with #{latin_number[i-1]}"
         end
 
@@ -57,10 +61,65 @@ module LatinToInteger
 
   def group_of(letter)
     if (LETTERARRAY.index(letter)+2)%2 == 0
-      return "X"
+      return :X
     elsif (LETTERARRAY.index(letter)+2)%2 == 1
-      return "V"
+      return :V
+    end
+  end
+
+  def too_many_times_group_x(group_current_letter, same_letter_counter, current_latin_number)
+    ### this method has for goal to verify that CCCC is impossible ####
+    # 1 the letter is of group x
+    # 2 the letter is more than three time
+    # 3 the letter is not "M"
+    if (group_current_letter == :X && same_letter_counter > 2 && current_latin_number != LETTERARRAY.last)
+      return true
+    end
+  end
+
+  def too_many_times_group_v(group_current_letter, same_letter_counter)
+    ### this method has for goal to verify that VV is impossible ####
+    # 1 the letter is of group v
+    # 2 the letter is more than one time
+    if (group_current_letter == :V && same_letter_counter > 0)
+      return true
+    end
+  end
+
+  def too_many_times_letter_m(current_latin_number, same_letter_counter)
+    ### this method has for goal to verify that MMMMM is impossible ####
+    # 1 the letter is "M"
+    # 2 the letter is more than 4 times
+
+    if (current_latin_number == LETTERARRAY.last && same_letter_counter > 3)
+      return true
+    end
+  end
+
+  def is_next_letter_incompatible_for_group_v(group_current_letter, current_latin_number, latin_number, i)
+    ### this method has for goal to verify that VX is impossible ####
+    # 1 the letter is of group v
+    # 2 the index of the current letter is smaller than the index of the next letter
+    if (group_current_letter == :V && LETTERARRAY.index(current_latin_number) < LETTERARRAY.index(latin_number[i-1]))
+      return true
+    end
+  end
+
+  def is_next_letter_incompatible_for_group_x(group_current_letter, current_latin_number, latin_number, i)
+    ### this method has for goal to verify that IC is impossible ####
+    # 1 the letter is of group x
+    # 2 the difference between the previous letter index and the current letter index is more than two
+    if (group_current_letter == :X && (LETTERARRAY.index(latin_number[i-1])-LETTERARRAY.index(current_latin_number) >= 3))
+      return true
+    end
+  end
+
+  def is_two_letter_of_group_v(group_current_letter, group_next_letter)
+    ### this method has for goal to verify that LV is impossible ####
+    # 1 the letter is of group V
+    # 2 the next letter is of group V
+    if (group_current_letter == :V && group_next_letter == :V)
+      return true
     end
   end
 end
-
